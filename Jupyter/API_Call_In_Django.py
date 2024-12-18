877bdef56cdd35dd3f45af3aa37d5639dc1f4fb0
Tushar Kumar <kumartushar78867@gmail.com>
2024-12-19 00:39:42 +0530

    anudip-training-work

--- Jupyter/API_Call_In_Django.py
+++ Jupyter/API_Call_In_Django.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render, HttpResponse
-import requests
+# from django.shortcuts import render, HttpResponse
+# import requests
 
-url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=modinagar&appid=8909baa00324102632b4fde2baa4d5f9'
-data = requests.get(url).json()
+# url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=modinagar&appid=8909baa00324102632b4fde2baa4d5f9'
+# data = requests.get(url).json()
 
-print(data)
+# print(data)
 
-payload = {
-        'city': data['name'], 
-        'weather': data['weather'][0]['main'],
-        # 'icon': data['weather'][0]['icons'],
-        'kelvin_temperature': data['main']['temp'],
-        'celcius_temperature': data['main']['temp'] - 273,
-        'pressure': data['main']['pressure'],
-        'humidity': data['main']['humidity']
-    }
+# payload = {
+#         'city': data['name'], 
+#         'weather': data['weather'][0]['main'],
+#         # 'icon': data['weather'][0]['icons'],
+#         'kelvin_temperature': data['main']['temp'],
+#         'celcius_temperature': data['main']['temp'] - 273,
+#         'pressure': data['main']['pressure'],
+#         'humidity': data['main']['humidity']
+#     }
     
-context = {'data': payload}
-print(context)    
+# context = {'data': payload}
+# print(context)    
---
+++ Z_practice/OodlesTechnology.py
@@ -0,0 +1,36 @@
+
+# value = eval(input("Enter the Value"))
+# print(value)
+# print(type(value))
+
+
+# list1 = [1,2,3,4,5] 
+# print(list1[::-1])
+
+# list1 = list((1,2,3,4,5))
+# print(list1)
+
+
+# tp = (1,2,3,4,5,6)
+# print(tp)
+# print(type(tp))
+
+# st = {1}
+# print(st)
+# print(type(st))
+
+# Step 1: Define the decorator function
+# def my_decorator(func):
+#     def wrapper():
+#         print("Before function execution.")
+#         func()  # Call the original function
+#         print("After function execution.")
+#     return wrapper
+
+# # Step 2: Use the decorator
+# @my_decorator
+# def say_hello():
+#     print("Hello, world!")
+
+# # Step 3: Call the decorated function
+# say_hello()
